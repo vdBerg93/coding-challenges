5acda7d927b9b7ce0896834033025ced01cc930f
van den Berg <vandenberg@sycada.com>
2023-12-08 19:36:13 +0100

    Day 7 part 2

---
+++ day8/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	var err error
+	data, err = os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	testData, err = os.ReadFile("sample.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func Test_Run(t *testing.T) {
+	run()
+}
